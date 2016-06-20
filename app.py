bf95c7ad54b772253571aa3a6480c2ab7931bd10
Inalberth Pinheiro <inalberth07@gmail.com>
2016-06-19 23:17:40 -0300

    Commit inicial

--- app.py
+++ app.py
@@ -61,5 +61,6 @@ def aeronaves():
 	return jsonify(result.data)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
